4402ea9f54445d4a00c2ce5572076eff38ca35df
fiveFloorMan <harvieusegithub@gmail.com>
2022-04-26 12:09:53 +0800

    handlebars初步建立完成

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
 app.get('/', (req, res) => {
-  res.send(`We are working`)
+  res.render('index')
 })
 
 app.listen(3000, () => {
