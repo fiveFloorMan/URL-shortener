7a58ce5a862cfd8c3689e26eb129aea3df99f6e7
fiveFloorMan <harvieusegithub@gmail.com>
2022-04-27 13:40:24 +0800

    判斷是否輸入空值

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
+const PORT = 3000
 const app = express()
 
 mongoose.connect('mongodb+srv://alpha:camp@cluster0.ow72u.mongodb.net/URL-shortener?retryWrites=true&w=majority')
@@ -18,6 +20,9 @@ db.once('open', () => {
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
 // 首頁
 app.get('/', (req, res) => {
   res.render('index')
@@ -25,17 +30,31 @@ app.get('/', (req, res) => {
 
 // create new URL(short)
 app.post('/', (req, res) => {
-  const finalRandomLetter = []
+
+  // 判斷是否有輸入網址
+  if(!req.body.origianlUrl){
+    return res.render('error')
+  }
+  // 產生隨機英數
+  const finalRandomLetterArray = []
   for (let i = 0 ; i < 5 ; i++){
     let RandomLetter = ""
+    // RandomLetter 是單一次的隨機英數
     RandomLetter = cutRandomLetter(getRandomNum(62))
-    finalRandomLetter.push(RandomLetter)
+    finalRandomLetterArray.push(RandomLetter)
+    // finalRandomLetterArray 是取出的隨機英數(Array)
   }
-  console.log('finalRandomLetter:', finalRandomLetter)
+  const finalRandomLetterString = finalRandomLetterArray.join()
+  const finalRandomLetter = finalRandomLetterString.replace(/,/g,"")
+  console.log('finalRandomLetter:',finalRandomLetter)
+  // finalRandomLetter 是最後的隨機英數
+
+  //判斷是否已經縮短過的網址
+  
 })
 
-app.listen(3000, () => {
-  console.log(`URL-shortener is working on localhost:3000`)
+app.listen(PORT, () => {
+  console.log(`URL-shortener is working on localhost:${PORT}`)
 })
 
 function getRandomNum(number){
@@ -43,6 +62,6 @@ function getRandomNum(number){
 }
 
 function cutRandomLetter(randomNum){
-const numberAndLetter = 'qazwsxedcrfvtgbyhnujmikolpQAZWSXEDCRFVTGBYHNUJMIKOLP1234567890'
-return numberAndLetter.charAt(randomNum)
+  const numberAndLetter = 'qazwsxedcrfvtgbyhnujmikolpQAZWSXEDCRFVTGBYHNUJMIKOLP1234567890'
+  return numberAndLetter.charAt(randomNum)
 }
